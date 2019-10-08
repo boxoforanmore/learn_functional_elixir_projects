def1fb64e4dd5f5d594481b5c9d031665f30d660
Peter Nielson <computergeekpete@gmail.com>
2019-10-08 17:12:47 -0600

    Added basic structs for rooms and actions

---
+++ dungeon_crawl/lib/dungeon_crawl/room/action.ex
@@ -0,0 +1,17 @@
+defmodule DungeonCrawl.Room.Action do
+    alias DungeonCrawl.Room.Action
+    defstruct label: nil,
+              id: nil
+
+    def forward do
+        %Action{id: :forward, label: "Move forward"}
+    end
+
+    def rest do
+        %Action{id: :rest, label: "Take a better look and rest"}
+    end
+
+    def search do
+        %Action{id: :search, label: "Search the room"}
+    end
+end
