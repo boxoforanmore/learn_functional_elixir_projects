d58e2087d5f9be291eec36224ae9ffa02448147c
Peter Nielson <computergeekpete@gmail.com>
2019-10-10 23:22:13 -0600

    Added way to show custom config working (p34)

--- ChapterOne/config_example/lib/config_example.ex
+++ ChapterOne/config_example/lib/config_example.ex
@@ -1,18 +1,26 @@
 defmodule ConfigExample do
-  @moduledoc """
-  Documentation for ConfigExample.
-  """
+    @moduledoc """
+    Documentation for ConfigExample.
+    """
+    @message_one Application.get_env(:config_example, :message_one)
+    @message_two Application.get_env(:config_example, :message_two)
 
-  @doc """
-  Hello world.
 
-  ## Examples
+    @doc """
+    Hello world.
 
-      iex> ConfigExample.hello()
-      :world
+    ## Examples
 
-  """
-  def hello do
-    :world
-  end
+        iex> ConfigExample.hello()
+        :world
+
+    """
+    def hello do
+        :world
+    end
+
+    def show_messages() do
+        IO.puts("Message one is: #{@message_one}")
+        IO.puts("Message two is: #{@message_two}")
+    end
 end
