aef219ad0809af1a4d312c661b34a4b7f6dc23cb
Peter Nielson <computergeekpete@gmail.com>
2019-10-10 22:57:09 -0600

    Added basic app for playing with configurations

---
+++ ChapterOne/config_example/lib/config_example.ex
@@ -0,0 +1,18 @@
+defmodule ConfigExample do
+  @moduledoc """
+  Documentation for ConfigExample.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ConfigExample.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
