9f6e3052dd7463bf19d6fc229337030e095785d1
Peter Nielson <computergeekpete@gmail.com>
2019-10-12 16:43:36 -0600

    Added escript option to indicate where the main function is

--- ChapterThree/word_frequency/mix.exs
+++ ChapterThree/word_frequency/mix.exs
@@ -7,6 +7,7 @@ defmodule WordFrequency.MixProject do
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
+      escript: [main_module: WordFreqeuncy],
       deps: deps()
     ]
   end
