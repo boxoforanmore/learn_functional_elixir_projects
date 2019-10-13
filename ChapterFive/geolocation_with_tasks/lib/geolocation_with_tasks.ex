621ffaa4b413e7be239da7095c0b2d10b506637e
Peter Nielson <computergeekpete@gmail.com>
2019-10-12 18:33:40 -0600

    Added basic geolocation app

---
+++ ChapterFive/geolocation_with_tasks/lib/geolocation_with_tasks.ex
@@ -0,0 +1,18 @@
+defmodule GeolocationWithTasks do
+  @moduledoc """
+  Documentation for GeolocationWithTasks.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GeolocationWithTasks.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ ChapterFive/geolocation_with_tasks/test/geolocation_with_tasks_test.exs
@@ -0,0 +1,8 @@
+defmodule GeolocationWithTasksTest do
+  use ExUnit.Case
+  doctest GeolocationWithTasks
+
+  test "greets the world" do
+    assert GeolocationWithTasks.hello() == :world
+  end
+end
