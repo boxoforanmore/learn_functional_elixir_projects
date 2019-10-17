c4da3848379d7602b8945a18987c9a766c0083c3
Peter Nielson <computergeekpete@gmail.com>
2019-10-16 23:01:25 -0600

    Added basic unit tests

---
+++ PhoenixWebDev/vocial/test/vocial_web/controllers/poll_controller_test.exs
@@ -0,0 +1,13 @@
+defmodule VocialWeb.PollControllerTest do
+  use VocialWeb.ConnCase
+
+  test "GET /", %{conn: conn} do
+    conn = get(conn, "/")
+    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+  end
+
+  test "GET /polls", %{conn: conn} do
+    conn = get(conn, "/polls")
+    assert html_response(conn, 200) =~ "My First Poll"
+  end
+end
